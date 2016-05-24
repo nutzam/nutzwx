617b999997474a1e23d52b54e286ceea87aa6858
wendal chen <wendal1985@gmail.com>
2016-05-24 21:27:43 +0800

    fixed #10 支持加密模式 add: 添加小视频类型
    Signed-off-by: wendal chen <wendal1985@gmail.com>

--- src/main/java/org/nutz/weixin/bean/WxMsgType.java
+++ src/main/java/org/nutz/weixin/bean/WxMsgType.java
@@ -1,7 +1,7 @@
 package org.nutz.weixin.bean;
 
 public enum WxMsgType {
-	text,image,voice,video,location,link,event,
+	text,image,voice,video,location,link,event,shortvideo,
 	// for out
 	music,news
 }
