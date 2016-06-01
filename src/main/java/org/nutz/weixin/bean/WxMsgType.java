2bb4de36948e3682c6ecf289dc82ace7c80c1e0b
wendal chen <wendal1985@gmail.com>
2016-06-01 21:33:04 +0800

    add: mpvideo,wxcard消息类型

--- src/main/java/org/nutz/weixin/bean/WxMsgType.java
+++ src/main/java/org/nutz/weixin/bean/WxMsgType.java
@@ -1,7 +1,7 @@
 package org.nutz.weixin.bean;
 
 public enum WxMsgType {
-	text,image,voice,video,location,link,event,shortvideo,
+	text,image,voice,video,location,link,event,shortvideo,mpvideo,wxcard,
 	// for out
 	music,news,mpnews,transfer_customer_service
 }
