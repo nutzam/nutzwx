f92e8684fbce3b72a3d16a62397049a4280c47c0
fanqun <hzfanqun@qq.com>
2016-06-02 06:13:10 +0800

    加了break

--- src/main/java/org/nutz/weixin/util/Wxs.java
+++ src/main/java/org/nutz/weixin/util/Wxs.java
@@ -573,8 +573,10 @@ public class Wxs {
 			break;
 		case mpnews:
 		    map.put("mpnews", new NutMap().setv("media_id", msg.getImage().getMediaId()));
+			break;
 		case wxcard:
 		    map.put("wxcard", new NutMap().setv("card_id", msg.getCard().getId()).setv("card_ext", msg.getCard().getExt()));
+			break;
 		default:
 			break;
 		}
