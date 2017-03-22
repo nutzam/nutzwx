1fc75f7c7f481f36649913c45eb9a71e9967a870
Wizzer <wizzer@qq.com>
2017-03-22 21:51:13 +0800

    fix: DaoAccessTokenStore

--- src/main/java/org/nutz/weixin/at/impl/DaoAccessTokenStore.java
+++ src/main/java/org/nutz/weixin/at/impl/DaoAccessTokenStore.java
@@ -7,53 +7,61 @@ import org.nutz.dao.Sqls;
 import org.nutz.dao.entity.Record;
 import org.nutz.dao.sql.Sql;
 import org.nutz.weixin.at.WxAccessToken;
+import org.nutz.weixin.spi.WxAccessTokenStore;
 
-public class DaoAccessTokenStore extends CacheableAccessTokenStore {
+public class DaoAccessTokenStore implements WxAccessTokenStore {
 
 	protected Dao dao;
 	protected Map<String, Object> params;
-	
-	protected String fetch = "select access_token from t_wx_at";
-	protected String update = "update t_wx_at set access_token=@token, access_token_expires=@access_token_expires";
-	
+
+	protected String fetch = "select access_token,access_token_expires,access_token_lastat from wx_config where id=@id";
+	protected String update = "update wx_config set access_token=@access_token, access_token_expires=@access_token_expires, access_token_lastat=@access_token_lastat where id=@id";
+
 	public DaoAccessTokenStore() {
 	}
-	
+
 	public DaoAccessTokenStore(Dao dao) {
 		super();
 		this.dao = dao;
 	}
 
-	public WxAccessToken _getAccessToken() {
+	@Override
+	public WxAccessToken get() {
 		Sql sql = Sqls.fetchRecord(fetch);
 		if (params != null)
 			sql.params().putAll(params);
 		dao.execute(sql);
-		WxAccessToken tmp = sql.getObject(Record.class).toPojo(WxAccessToken.class);
-		return tmp;
+		Record record = sql.getObject(Record.class);
+		WxAccessToken at = new WxAccessToken();
+		at.setExpires(record.getInt("access_token_expires"));
+		at.setLastCacheTimeMillis(record.getLong("access_token_lastat"));
+		at.setToken(record.getString("access_token"));
+		return at;
 	}
 
-	public void _saveAccessToken(String token, int time) {
+	@Override
+	public void save(String token, int time, long lastCacheTimeMillis) {
 		Sql sql = Sqls.create(update);
 		if (params != null)
 			sql.params().putAll(params);
-		sql.params().set("token", token);
+		sql.params().set("access_token", token);
 		sql.params().set("access_token_expires", time);
+		sql.params().set("access_token_lastat", lastCacheTimeMillis);
 		dao.execute(sql);
 	}
 
 	public void setParams(Map<String, Object> params) {
 		this.params = params;
 	}
-	
+
 	public void setDao(Dao dao) {
 		this.dao = dao;
 	}
-	
+
 	public void setFetch(String fetch) {
 		this.fetch = fetch;
 	}
-	
+
 	public void setUpdate(String update) {
 		this.update = update;
 	}
